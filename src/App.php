a6f33a986d14ee6d6f59ab753467a6f94304aba0
i-do-dev <i.do.dev@outlook.com>
2020-07-06 00:43:55 -0700

    playlist view redirect url update

--- src/App.php
+++ src/App.php
@@ -35,7 +35,7 @@ class App
             if ( property_exists($lti_claim_custom, 'playlist') ) {
                 $playlist_id = $lti_claim_custom->playlist;
                 $lti_token_params = http_build_query($_SESSION['lti_post']);
-                $playlist_studio_link = CURRIKI_STUDIO_HOST."/playlist/preview/$playlist_id/resource/5eebbfbcc9bda1111d7be7e3";
+                $playlist_studio_link = CURRIKI_STUDIO_HOST."/playlist/lti/preview/$playlist_id";
                 $redirect_to_studio_url = $playlist_studio_link . "?" . $lti_token_params;            
                 header("Location: $redirect_to_studio_url");
             }else{
