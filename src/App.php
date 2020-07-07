2ed9484fe9e9b5efadcad7384821d07aab27e492
i-do-dev <i.do.dev@outlook.com>
2020-07-06 23:40:37 -0700

    playlist id support in query string with custom lti params

--- src/App.php
+++ src/App.php
@@ -27,16 +27,11 @@ class App
 
             //exectute LTI Launch                        
             $LTI = LTIX::requireData();
-            $lti_jwt = $_SESSION['tsugi_jwt'];
-            $lti_claim_custom_url = "https://purl.imsglobal.org/spec/lti/claim/custom";
-            $lti_claim_custom = $lti_jwt->body->{$lti_claim_custom_url};
-
-            //check if lti launch has 'playlist' custom pramameter with Id
-            if ( property_exists($lti_claim_custom, 'playlist') ) {
-                $playlist_id = $lti_claim_custom->playlist;
+            $playlist_id = ParamValidate::playlistInCustom($_SESSION) ?: ParamValidate::playlistInQueryString($_SESSION);
+            if ($playlist_id) {
                 $lti_token_params = http_build_query($_SESSION['lti_post']);
                 $playlist_studio_link = CURRIKI_STUDIO_HOST."/playlist/lti/preview/$playlist_id";
-                $redirect_to_studio_url = $playlist_studio_link . "?" . $lti_token_params;            
+                $redirect_to_studio_url = $playlist_studio_link . "?" . $lti_token_params;
                 header("Location: $redirect_to_studio_url");
             }else{
                 echo "<h1>Curriki LTI Tool</h1>";
