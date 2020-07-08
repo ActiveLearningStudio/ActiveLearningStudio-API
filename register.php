5f47c5ea51a2a55ed0c2e477873499d8cc71347a
i-do-dev <i.do.dev@outlook.com>
2020-07-08 01:29:51 -0700

    tool display details update

--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <?php
 
 $REGISTER_LTI2 = array(
-"name" => "Curriki",
+"name" => "CurrikiStudio",
 "FontAwesome" => "fa-graduation-cap",
-"short_name" => "Curriki",
-"description" => "This tool provide Curriki Programs/Playlist/Activity",
+"short_name" => "CurrikiStudio",
+"description" => "This tool serve CurrikiStudio Programs / Playlists / Activites.",
 "messages" => array("launch", "launch_message")
 );
 
