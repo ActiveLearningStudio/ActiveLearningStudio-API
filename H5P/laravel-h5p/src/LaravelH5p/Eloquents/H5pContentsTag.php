33ead52272876a38716ec4938879eedfae8779d1
iDoDev <63515864+i-do-dev@users.noreply.github.com>
2020-08-29 01:13:53 +0500

    H5p api (#5)

---
+++ H5P/laravel-h5p/src/LaravelH5p/Eloquents/H5pContentsTag.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Djoudi\LaravelH5p\Eloquents;
+
+use Illuminate\Database\Eloquent\Model;
+
+class H5pContentsTag extends Model {
+
+	protected $primaryKey = ['content_id', 'tag_id'];
+	protected $fillable = [
+		'content_id',
+		'tag_id',
+	];
+
+}
