affa7a0aad836dbdca63280570172b29707a4967
i-do-dev <i.do.dev@outlook.com>
2020-08-28 23:12:08 +0500

    H5P Models

---
+++ H5P/laravel-h5p/src/LaravelH5p/Eloquents/H5pTmpfile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Djoudi\LaravelH5p\Eloquents;
+
+use Illuminate\Database\Eloquent\Model;
+
+class H5pTmpfile extends Model {
+
+	protected $primaryKey = 'id';
+	protected $fillable = [
+		'path',
+		'created_at',
+	];
+
+}
