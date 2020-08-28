24da72862c5237454a8f23b9c5e998600ed4cedc
i-do-dev <i.do.dev@outlook.com>
2020-08-28 14:41:41 +0500

    return resource collection in response + API blocks

--- app/Http/Controllers/Api/V1/CurrikiGo/LmsSettingController.php
+++ app/Http/Controllers/Api/V1/CurrikiGo/LmsSettingController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1\CurrikiGo;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\V1\CurrikiGo\LmsSettingResource;
+use App\Http\Resources\V1\CurrikiGo\LmsSettingCollection;
 use App\Models\CurrikiGo\LmsSetting;
 use App\Repositories\CurrikiGo\LmsSetting\LmsSettingRepositoryInterface;
 use Illuminate\Http\Request;
@@ -11,6 +11,12 @@ use Illuminate\Http\Response;
 use Validator;
 use Redirect;
 
+/**
+ * @group  LMS Settings
+ * @authenticated
+ *
+ * APIs for LMS settings used for publishing
+ */
 class LmsSettingController extends Controller
 {
     private $lmsSettingRepository;
@@ -18,7 +24,7 @@ class LmsSettingController extends Controller
     /**
      * LmsSettingController constructor.
      *
-     * @param LmsSettingRepositoryInterface $lmsSettingRepository
+     * @bodyParam $lmsSettingRepository LmsSettingRepositoryInterface required repository functions
      */
     public function __construct(LmsSettingRepositoryInterface $lmsSettingRepository) {
         $this->lmsSettingRepository = $lmsSettingRepository;
@@ -26,13 +32,16 @@ class LmsSettingController extends Controller
     }
 
     /**
-     * Display a listing of the LMS settings for authenticated user.
+     * Authenticated user LMS settings
+     * 
+     * Display a listing of the LMS settings for authenticated user
      *
-     * @return Response
+     * @apiResourceCollection   App\Http\Resources\V1\CurrikiGo\LmsSettingCollection
+     * @apiResourceModel    App\Models\CurrikiGo\LmsSetting
      */
     public function my(){
         $lms_settings = $this->lmsSettingRepository->fetchAllByUserId(auth()->user()->id);
-        return response( ['lms_settings' => LmsSettingResource::collection($lms_settings)], 200);
+        return response(new LmsSettingCollection($lms_settings), 200);
     }
 
 }
