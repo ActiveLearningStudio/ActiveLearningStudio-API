481ab0bf9794f339cbfe311367e9e842c53c6358
i-do-dev <i.do.dev@outlook.com>
2020-08-29 23:45:30 +0500

    h5p indent fix

--- H5P/laravel-h5p/routes/laravel-h5p.php
+++ H5P/laravel-h5p/routes/laravel-h5p.php
@@ -37,7 +37,7 @@ Route::group(['middleware' => ['web']], function () {
 	}
 
 	// export
-	//    if (config('laravel-h5p.use_router') == 'EXPORT' || config('laravel-h5p.use_router') == 'ALL') {
+	//if (config('laravel-h5p.use_router') == 'EXPORT' || config('laravel-h5p.use_router') == 'ALL') {
 	Route::get('h5p/embed/{id}', 'Djoudi\LaravelH5p\Http\Controllers\EmbedController')->name("h5p.embed");
 	Route::get('h5p/export/{id}', 'Djoudi\LaravelH5p\Http\Controllers\DownloadController')->name("h5p.export");
 	Route::get('h5p/content-params/{id}', "Djoudi\LaravelH5p\Http\Controllers\H5pController@contentParams");
