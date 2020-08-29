723faa75c40b3f6f5fabe38da536496f6b793001
i-do-dev <i.do.dev@outlook.com>
2020-08-29 15:14:57 +0500

    exclude a package from provider

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
          * Package Service Providers...
          */
         Djoudi\LaravelH5p\LaravelH5pServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
