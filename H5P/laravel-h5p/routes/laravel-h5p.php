f825ea8c24cf6195541913bda980b32d3137fe95
i-do-dev <i.do.dev@outlook.com>
2020-09-04 23:13:27 +0500

    removed extra/commented h5p routes

--- H5P/laravel-h5p/routes/laravel-h5p.php
+++ H5P/laravel-h5p/routes/laravel-h5p.php
@@ -1,13 +1,5 @@
 <?php
 
-/*Route::group(['middleware' => ['api']], function () {
-	Route::resource('api/h5p', "Djoudi\LaravelH5p\Http\Controllers\H5pApiController");
-});*/
-
-/*Route::group(['middleware' => ['api', 'auth:api', 'verified']], function () {
-	//Route::get('/api/h5p/ajax/libraries', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@libraries')->name("h5p.ajax.libraries");
-});*/
-
 Route::group(['middleware' => ['api']], function () {
 	if (config('laravel-h5p.use_router') == 'EDITOR' || config('laravel-h5p.use_router') == 'ALL') {
 		Route::group(['middleware' => ['auth']], function () {
@@ -21,25 +13,6 @@ Route::group(['middleware' => ['api']], function () {
 		});
 	}
 
-	// export
-	// ajax
-	//Route::match(['GET', 'POST'], 'h5p/ajax/libraries', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@libraries')->name("h5p.ajax.libraries");
-	//Route::get('h5p/ajax', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController')->name("h5p.ajax");
-	//Route::get('h5p/ajax/libraries', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@libraries')->name("h5p.ajax.libraries");
-	//Route::get('h5p/ajax/single-libraries', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@singleLibrary')->name("h5p.ajax.single-libraries");
-	//Route::any('h5p/ajax/content-type-cache', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@contentTypeCache')->name("h5p.ajax.content-type-cache");
-	//Route::any('h5p/ajax/library-install', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@libraryInstall')->name("h5p.ajax.library-install");
-	//Route::post('h5p/ajax/library-upload', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@libraryUpload')->name("h5p.ajax.library-upload");
-	//Route::post('h5p/ajax/rebuild-cache', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@rebuildCache')->name("h5p.ajax.rebuild-cache");
-	//Route::any('h5p/ajax/files', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@files')->name("h5p.ajax.files");
-	//Route::any('h5p/ajax/filter', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@filter')->name("h5p.ajax.filter");
-	//Route::any('h5p/ajax/finish', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@finish')->name("h5p.ajax.finish");
-	//Route::any('h5p/ajax/content-user-data', 'Djoudi\LaravelH5p\Http\Controllers\AjaxController@contentUserData')->name("h5p.ajax.content-user-data");
-	//Route::any('h5p-result/my', 'Djoudi\LaravelH5p\Http\Controllers\H5PResultController@my')->name("h5p.result.my");
-
-	//if(config('laravel-h5p.use_router') == 'EXPORT' || config('laravel-h5p.use_router') == 'ALL') {
 	Route::get('h5p/embed/{id}', 'Djoudi\LaravelH5p\Http\Controllers\EmbedController')->name("h5p.embed");
-	Route::get('h5p/export/{id}', 'Djoudi\LaravelH5p\Http\Controllers\DownloadController')->name("h5p.export");
 	Route::get('h5p/content-params/{id}', "Djoudi\LaravelH5p\Http\Controllers\H5pController@contentParams");
-	//}
 });
