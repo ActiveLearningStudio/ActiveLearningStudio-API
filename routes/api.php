b492319510ebdbfae62713462fcd4f7bccf833d5
Yury Sakovich <yurasakovich29@gmail.com>
2020-09-10 14:55:14 +0200

    Update change password route

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\V1'], function () {
     Route::middleware(['auth:api', 'verified'])->group(function () {
         Route::post('subscribe', 'UserController@subscribe');
         Route::get('users/me', 'UserController@me');
-        Route::get('users/update-password', 'UserController@updatePassword');
+        Route::post('users/update-password', 'UserController@updatePassword');
         Route::apiResource('users', 'UserController');
 
         Route::post('projects/upload-thumb', 'ProjectController@uploadThumb');
