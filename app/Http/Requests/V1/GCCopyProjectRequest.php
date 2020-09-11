97f0353f5dba262ea0288c1634050753a095eadc
yurysakovich1010 <67044861+yurysakovich1010@users.noreply.github.com>
2020-09-11 13:35:59 +0300

    Added a PlaylistRequest (#73)

--- app/Http/Requests/V1/GCCopyProjectRequest.php
+++ app/Http/Requests/V1/GCCopyProjectRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class GCSaveAccessTokenRequest extends FormRequest
+class GCCopyProjectRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class GCSaveAccessTokenRequest extends FormRequest
     public function rules()
     {
         return [
-            'access_token' => 'required'
+            'course_id' => 'sometimes|integer'
         ];
     }
 }
--- app/Http/Requests/V1/SearchRequest.php
+++ app/Http/Requests/V1/SearchRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
