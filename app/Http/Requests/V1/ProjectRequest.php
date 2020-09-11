c784d599156f2ffb4e36c7b1be15ed3695a4eb9a
aqeel-tkxel <aqeel.ahmad@tkxel.com>
2020-09-11 21:13:43 +0500

    CUR-480 - Server Validation added for Project Add/Edit through Reques… (#74)
    * CUR-480 - Server Validation added for Project Add/Edit through Request Form validation.
    
    * CUR-480 - corrected function definition for update().

---
+++ app/Http/Requests/V1/ProjectRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:80',
+            'description' => 'required|string|max:1000',
+            'thumb_url' => 'required',
+            'is_public' => 'required',
+        ];
+    }
+}
