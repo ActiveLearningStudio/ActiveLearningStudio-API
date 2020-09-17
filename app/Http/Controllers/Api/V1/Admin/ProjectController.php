e00b98d9f6a698a0efa323e8b9e08ef2c099c2af
Ahmad Mukhtar <ahmad.curriki@gmail.com>
2020-09-17 18:20:23 +0500

    Get project data for preview/share

--- app/Http/Controllers/Api/V1/Admin/ProjectController.php
+++ app/Http/Controllers/Api/V1/Admin/ProjectController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1\Admin;
 use App\Exceptions\GeneralException;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\Admin\ProjectResource;
+use App\Models\Project;
 use App\Repositories\Admin\Project\ProjectRepository;
+use App\Repositories\Project\ProjectRepositoryInterface;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
@@ -41,7 +43,32 @@ class ProjectController extends Controller
      * @return Application|ResponseFactory|Response
      * @throws GeneralException
      */
-    public function updateIndexes(Request $request, $id){
-        return response(['message' =>  $this->projectRepository->updateIndexes($request->projects, $id)], 200);
+    public function updateIndexes(Request $request, $id)
+    {
+        return response(['message' => $this->projectRepository->updateIndexes($request->projects, $id)], 200);
     }
+
+    /**
+     * @param Project $project
+     * @return Application|ResponseFactory|Response
+     */
+    public function updateIndex(Project $project)
+    {
+        \Log::info($project);
+        return response(['message' => $this->projectRepository->updateIndex($project)], 200);
+    }
+
+    /**
+     * Display the specified project.
+     *
+     * @param Project $project
+     * @return Response
+     */
+    public function loadShared(Project $project)
+    {
+        return response([
+            'data' => resolve(ProjectRepositoryInterface::class)->getProjectForPreview($project),
+        ], 200);
+    }
+
 }
