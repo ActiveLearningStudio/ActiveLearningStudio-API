5990513d04aa807f874fbca4dfe3f5889a06fcfa
Ahmad Mukhtar <ahmad.curriki@gmail.com>
2020-09-17 20:59:24 +0500

    Index/de-index related tables data

--- app/Repositories/Admin/Project/ProjectRepository.php
+++ app/Repositories/Admin/Project/ProjectRepository.php
@@ -179,8 +179,11 @@ class ProjectRepository extends BaseRepository
      */
     public function updateIndex($project): string
     {
-        $project->elasticsearch = ! $project->elasticsearch;
-        $project->save();
+        if ($project->elasticsearch) {
+            $this->de_index_projects([$project->id]);
+        } else {
+            $this->index_projects([$project->id]);
+        }
         return 'Index Status Changed Successfully!';
     }
 }
