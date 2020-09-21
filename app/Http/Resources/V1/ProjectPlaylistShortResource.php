1bc90d346fb289c9172b882a53de003f81e4c60c
i-do-dev <i.do.dev@outlook.com>
2020-09-21 14:01:07 +0500

    projects short result set for lti deeplink

---
+++ app/Http/Resources/V1/ProjectPlaylistShortResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectPlaylistShortResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'order' => $this->order,
+            'activities' => PlaylistActivityShortResource::collection($this->activities->sortBy('order')),
+            'created_at' => $this->created_at
+        ];
+    }
+}
