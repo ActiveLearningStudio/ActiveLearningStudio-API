17ccd937bed6f648756e3df0d29d2340cc4ef7b9
Mahmood <mahmood.westanalytics@nxvt.com>
2020-10-09 17:00:16 +0500

    reordering of projects

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\V1'], function () {
         Route::post('projects/{project}/share', 'ProjectController@share');
         Route::post('projects/{project}/clone', 'ProjectController@clone');
         Route::post('projects/{project}/remove-share', 'ProjectController@removeShare');
-        Route::post('projects/{project}/reorder', 'ProjectController@reorder');
+        Route::post('projects/reorder', 'ProjectController@reorder');
         Route::apiResource('projects', 'ProjectController');
 
         Route::post('projects/{project}/playlists/reorder', 'PlaylistController@reorder');
