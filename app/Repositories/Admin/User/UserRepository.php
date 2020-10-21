c506b4cf12bf6d9201df2647298856fd8a7ed2fe
Ahmad Mukhtar <ahmad.curriki@gmail.com>
2020-10-21 14:08:42 +0500

    assign and remove user role

--- app/Repositories/Admin/User/UserRepository.php
+++ app/Repositories/Admin/User/UserRepository.php
@@ -155,7 +155,7 @@ class UserRepository extends BaseRepository
     /**
      * @param $data
      * @return array
-     * @throws GeneralException
+     * @throws GeneralException|\Throwable
      */
     public function bulkImport($data)
     {
@@ -209,5 +209,22 @@ class UserRepository extends BaseRepository
             Log::error(implode(", ", $errors));
         }
         return $report;
+    }
+
+    /**
+     * @param $user
+     * @param $role
+     * @return string
+     * @throws GeneralException
+     */
+    public function updateRole($user, $role): string
+    {
+        if ($user->id === auth()->id()) {
+            throw new GeneralException('You cannot change the role of yourself.');
+        }
+        $role = $role ? 'admin' : null;
+        Log::error($role);
+        $user->update(['role' => $role]);
+        return "User role is changed successfully!";
     }
 }
