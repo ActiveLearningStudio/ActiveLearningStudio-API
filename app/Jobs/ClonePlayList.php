a6c705bbea744aa8316092ad6b8454f0899c1853
Ahmad Mukhtar <68377680+ahmad-curriki@users.noreply.github.com>
2020-10-22 13:03:40 +0500

    Clone Projects, playlists jobs exception handling (#160)

--- app/Jobs/ClonePlayList.php
+++ app/Jobs/ClonePlayList.php
@@ -57,14 +57,18 @@ class ClonePlayList implements ShouldQueue
      */
     public function handle(PlaylistRepositoryInterface $playlistRepository, UserRepositoryInterface $userRepository)
     {
-        $playlistRepository->clone($this->project, $this->playlist, $this->token);
-        $isDuplicate = ($this->playlist->project_id == $this->project->id);
-        $process = ($isDuplicate) ? "duplicate" : "clone";
-        $message =  "Your request to $process playlist [".$this->playlist->title."] has been completed and available" ;
-        (new \App\Events\SendMessage($message));
-        $user_id = $userRepository->parseToken($this->token);
-        $user = User::find($user_id);
-        $userName = rtrim($user->first_name . ' ' . $user->last_name, ' ');
-        $user->notify(new CloneNotification($message, $process, $userName));
+        try {
+            $playlistRepository->clone($this->project, $this->playlist, $this->token);
+            $isDuplicate = ($this->playlist->project_id == $this->project->id);
+            $process = ($isDuplicate) ? "duplicate" : "clone";
+            $message = "Your request to $process playlist [" . $this->playlist->title . "] has been completed and available";
+            (new \App\Events\SendMessage($message));
+            $user_id = $userRepository->parseToken($this->token);
+            $user = User::find($user_id);
+            $userName = rtrim($user->first_name . ' ' . $user->last_name, ' ');
+            $user->notify(new CloneNotification($message, $process, $userName));
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
     }
 }
