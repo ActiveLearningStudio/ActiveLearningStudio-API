5ba832d4d3f7503e3a7fdef46e0b476248c249a3
CptTony <antonioetayo@gmail.com>
2020-10-26 10:45:26 -0400

    Cur 578 (#163)
    * Bugfix to project list
    * Added User LMS listings

---
+++ app/Repositories/UserLmsSettings/UserLmsSettingsRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\UserLmsSettings;
+
+interface UserLmsSettingsRepositoryInterface
+{
+    /**
+     * Get all lms settings for the user
+     */
+    public function all();
+}
