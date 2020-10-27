d2b6142d9974e4938f9308bcf7ced766884dd4b3
Yury Sakovich <yurasakovich29@gmail.com>
2020-10-27 08:30:40 +0100

    Fix namespace issue in TeamController

--- app/Http/Controllers/Api/V1/TeamController.php
+++ app/Http/Controllers/Api/V1/TeamController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Events\TeamCreatedEvent;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\v1\TeamAddMemberRequest;
+use App\Http\Requests\V1\TeamAddMemberRequest;
 use App\Http\Requests\V1\TeamAddProjectRequest;
 use App\Http\Requests\V1\TeamInviteMemberRequest;
 use App\Http\Requests\V1\TeamInviteRequest;
