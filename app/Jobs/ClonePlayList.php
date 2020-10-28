626b400a6b68369f852485a10ff0c598c94df980
Mahmood <mahmood.westanalytics@nxvt.com>
2020-10-29 01:09:00 +0500

    Feauture-auth-to-private-channel

--- app/Jobs/ClonePlayList.php
+++ app/Jobs/ClonePlayList.php
@@ -62,8 +62,8 @@ class ClonePlayList implements ShouldQueue
             $isDuplicate = ($this->playlist->project_id == $this->project->id);
             $process = ($isDuplicate) ? "duplicate" : "clone";
             $message = "Your request to $process playlist [" . $this->playlist->title . "] has been completed and available";
-            broadcast(new \App\Events\SendMessage($message));
             $user_id = $userRepository->parseToken($this->token);
+            broadcast(new \App\Events\SendMessage($message, $user_id));
             $user = User::find($user_id);
             $userName = rtrim($user->first_name . ' ' . $user->last_name, ' ');
             $user->notify(new CloneNotification($message, $process, $userName));
