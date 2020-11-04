9f652165331f0b10c9b29c5e545379c0f307992f
Yury Sakovich <yurasakovich29@gmail.com>
2020-11-04 17:59:21 +0100

    Update API doc

--- resources/views/apidoc/index.blade.php
+++ resources/views/apidoc/index.blade.php
@@ -6,45 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>API Reference</title>
 
-    <link rel="stylesheet" href="{{ asset('/docs/css/style.css') }}" />
-    <script src="{{ asset('/docs/js/all.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/api/docs/css/style.css') }}" />
+    <script src="{{ asset('/api/docs/js/all.js') }}"></script>
 
-
-          <script>
+    <script>
         $(function() {
             setupLanguages(["bash","javascript","php","python"]);
         });
-      </script>
-      </head>
+    </script>
+</head>
 
-  <body class="">
+<body class="">
     <a href="#" id="nav-button">
-      <span>
-        NAV
-        <img src="/docs/images/navbar.png" />
-      </span>
+        <span>
+            NAV
+            <img src="/api/docs/images/navbar.png" />
+        </span>
     </a>
     <div class="tocify-wrapper">
-        <img src="/docs/images/logo.png" />
-                    <div class="lang-selector">
-                                  <a href="#" data-language-name="bash">bash</a>
-                                  <a href="#" data-language-name="javascript">javascript</a>
-                                  <a href="#" data-language-name="php">php</a>
-                                  <a href="#" data-language-name="python">python</a>
-                            </div>
-                            <div class="search">
-              <input type="text" class="search" id="input-search" placeholder="Search">
-            </div>
-            <ul class="search-results"></ul>
-              <div id="toc">
-      </div>
-                    <ul class="toc-footer">
-                                  <li><a href='http://github.com/mpociot/documentarian'>Documentation Powered by Documentarian</a></li>
-                            </ul>
-            </div>
+        <img src="/api/docs/images/logo.png" />
+        <div class="lang-selector">
+            <a href="#" data-language-name="bash">bash</a>
+            <a href="#" data-language-name="javascript">javascript</a>
+            <a href="#" data-language-name="php">php</a>
+            <a href="#" data-language-name="python">python</a>
+        </div>
+        <div class="search">
+            <input type="text" class="search" id="input-search" placeholder="Search">
+        </div>
+        <ul class="search-results"></ul>
+        <div id="toc">
+        </div>
+        <ul class="toc-footer">
+            <li><a href='http://github.com/mpociot/documentarian'>Documentation Powered by Documentarian</a></li>
+        </ul>
+    </div>
     <div class="page-wrapper">
-      <div class="dark-box"></div>
-      <div class="content">
+        <div class="dark-box"></div>
+        <div class="content">
           <!-- START_INFO -->
 <h1>Info</h1>
 <p>Welcome to the generated API reference.
