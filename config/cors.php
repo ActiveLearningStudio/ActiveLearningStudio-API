4f499eead449ba7d2d9e0e2e856b712f447416a6
Yury Sakovich <yurasakovich29@gmail.com>
2020-11-18 19:33:04 +0100

    Update CORS config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('APP_ALLOWED_URL', '*')],
+    'allowed_origins' => ['*'], // [env('APP_ALLOWED_URL', '*')],
 
     'allowed_origins_patterns' => [],
 
