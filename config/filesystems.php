7e29b231076e507e3ba60f069c5f9b3bf0ef4cd0
Ahmad Mukhtar <ahmad.curriki@gmail.com>
2020-11-18 15:25:28 +0500

    configuration for minio S3

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,6 +65,15 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            'endpoint' => env('MINIO_ENDPOINT', 'http://127.0.0.1:9000'),
+            'use_path_style_endpoint' => true,
+            'key' => env('MINIO_KEY'),
+            'secret' => env('MINIO_SECRET'),
+            'region' => env('MINIO_REGION'),
+            'bucket' => env('MINIO_BUCKET'),
+        ],
     ],
 
     /*
