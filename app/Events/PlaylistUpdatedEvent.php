9bb3c8be258339ca6da9bc88f590e293f285d697
Yury Sakovich <yurasakovich29@gmail.com>
2020-11-20 00:53:36 +0100

    Authorize channels

--- app/Events/PlaylistUpdatedEvent.php
+++ app/Events/PlaylistUpdatedEvent.php
@@ -6,10 +6,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PlaylistUpdatedEvent implements ShouldBroadcast
+class PlaylistUpdatedEvent implements ShouldBroadcastNow
 {
 
     use Dispatchable, InteractsWithSockets, SerializesModels;
