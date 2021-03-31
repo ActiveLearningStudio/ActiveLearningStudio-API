908790c1f1d90de875034a63639142a6b117ec9e
Fahad <fahad.curriki@nxvt.com>
2021-03-31 17:29:15 +0500

    Added seeder for Organization Role Type for Super Admin. Updated OrganizationRolePermission seeder to get role id by name.

--- database/seeds/OrganizationRolePermissionSeeder.php
+++ database/seeds/OrganizationRolePermissionSeeder.php
@@ -14,14 +14,18 @@ class OrganizationRolePermissionSeeder extends Seeder
         $organizationPermissionTypes = DB::table('organization_permission_types')->select('id', 'feature')->get();
 
         foreach ($organizationPermissionTypes as $organizationPermissionType) {
+            $adminRole = DB::table('organization_role_types')->where('name', 'admin')->first();
+
             DB::table('organization_role_permissions')->insert([
-                'organization_role_type_id' => 2,
+                'organization_role_type_id' => $adminRole->id,
                 'organization_permission_type_id' => $organizationPermissionType->id
             ]);
 
+            $courseCreatorRole = DB::table('organization_role_types')->where('name', 'course_creator')->first();
+
             if (in_array($organizationPermissionType->feature, ['Project', 'Playlist', 'Activity', 'Team', 'Group', 'Search'])) {
                 DB::table('organization_role_permissions')->insert([
-                    'organization_role_type_id' => 3,
+                    'organization_role_type_id' => $courseCreatorRole->id,
                     'organization_permission_type_id' => $organizationPermissionType->id
                 ]);
             }
@@ -29,9 +33,11 @@ class OrganizationRolePermissionSeeder extends Seeder
 
             $memberPermissions = ['12', '14', '15', '17', '18', '21', '23', '24', '27', '29', '30', '33', '45', '55', '56'];
 
+            $memberRole = DB::table('organization_role_types')->where('name', 'member')->first();
+
             if (in_array($organizationPermissionType->id, $memberPermissions)) {
                 DB::table('organization_role_permissions')->insert([
-                    'organization_role_type_id' => 4,
+                    'organization_role_type_id' => $memberRole->id,
                     'organization_permission_type_id' => $organizationPermissionType->id
                 ]);
             }
