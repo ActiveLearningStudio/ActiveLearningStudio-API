bcd450c161ad1c915eaf6b1288cd1da100a0e98a
Fahad <fahad.curriki@nxvt.com>
2021-04-21 15:17:53 +0500

    Fixed org multi tenancy seeder.

--- database/seeds/OrganizationSeeder.php
+++ database/seeds/OrganizationSeeder.php
@@ -18,15 +18,5 @@ class OrganizationSeeder extends Seeder
         ]);
 
         DB::table('projects')->update(['organization_id' => 1, 'organization_visibility_type_id' => 4]);
-
-        $users = DB::table('users')->select('id')->get();
-
-        foreach ($users as $user) {
-            DB::table('organization_user_roles')->insert([
-                'organization_id' => 1,
-                'user_id' => $user->id,
-                'organization_role_type_id' => 3
-            ]);
-        }
     }
 }
