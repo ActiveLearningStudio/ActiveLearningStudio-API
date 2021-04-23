438e41148b7cffa578e2edbedca1739c01f71716
IMCoder <imcoder001@gmail.com>
2021-04-24 04:06:39 +0500

    docker updates for auto builds

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # service apache2 restart &
+php /var/www/html/artisan config:cache
+php /var/www/html/artisan storage:link
+php /var/www/html/artisan passport:install
 service cron start &
 redis-server &
 laravel-echo-server start --force &
