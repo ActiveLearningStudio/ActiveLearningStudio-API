df27db8031f42de9b7ffcac150aca4420fd8eef9
Ali Mehdi <imcoder001@gmail.com>
2021-05-03 23:06:57 +0500

    Update entrypoint.esmigrate.sh

--- entrypoint.esmigrate.sh
+++ entrypoint.esmigrate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-php /var/www/html/artisan config:cache
+print yes | php /var/www/html/artisan elastic:migrate:refresh
+php /var/www/html/artisan scout:import "App\Models\Activity"
+php /var/www/html/artisan scout:import "App\Models\Playlist"
+php /var/www/html/artisan scout:import "App\Models\Project"
 
-
-touch /var/www/html/health.ok
-
-apache2ctl -D FOREGROUND
 # while true; do sleep 1000000000000; done
