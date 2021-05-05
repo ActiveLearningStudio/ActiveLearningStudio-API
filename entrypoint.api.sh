a18935bf1a8efaa66cc132f89761ff04864ff277
Ali Mehdi <imcoder001@gmail.com>
2021-05-05 22:04:25 +0500

    Update entrypoint.api.sh

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -4,18 +4,18 @@
 chmod 777 -R /var/www/html/storage
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan storage:link
-if [[ ! -e /var/www/html/oauth-private.key || ! -e /var/www/html/oauth-public.key ]]; then php /var/www/html/artisan passport:install; fi
+if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage/oauth-public.key ]]; then php /var/www/html/artisan passport:install; fi
 
 
 #temporary for redis
-#service cron start &
-#redis-server &
+service cron start &
+redis-server &
 #temporary for redis
-#sleep 20
+sleep 20
 #temporary for redis
-#laravel-echo-server start --force &
+laravel-echo-server start --force &
 #temporary for redis
-#php /var/www/html/artisan queue:work --timeout=0 &
+php /var/www/html/artisan queue:work --timeout=0 &
 touch /var/www/html/health.ok
 
 apache2ctl -D FOREGROUND
