49420548f91e1497fd11ba9635821bafa2e40f34
Antonio Etayo <antonioetayo@gmail.com>
2021-07-05 13:14:52 -0400

    implemented xapi zip file download feature

--- app/Services/CurrikiGo/LMSIntegrationService.php
+++ app/Services/CurrikiGo/LMSIntegrationService.php
@@ -2,10 +2,12 @@
 
 namespace App\Services\CurrikiGo;
 
+use Illuminate\Support\Str;
 use App\Services\CurrikiGo\LMSIntegrationServiceInterface;
 use App\Services\CurrikiGo\TalentLMS\TalentLMS;
 use App\Models\CurrikiGo\LmsSetting;
 use App\Models\Project;
+use App\Models\Activity;
 
 /**
  * Generic LMS Integration Service
@@ -48,4 +50,23 @@ class LMSIntegrationService implements LMSIntegrationServiceInterface
 		$this->lms = new $this->targets[$targetLms]($setting);
 		return $this->lms->publishProject($project);
 	}
+
+    /**
+     * Get XAPI file contents
+     * 
+     * @return string
+     */
+    public function getXAPIFile(Activity $activity)
+    {
+    	$filename = 'storage/xapifiles/'.Str::slug($activity->title, '-').'.zip';
+    	$url = config('app.front_end_url').'/activity/'.$activity->id.'/shared';
+    	$html = view('api.xapihtml', ['url' => $url, 'title' => $activity->title])->render();
+    	$xml = view('api.xapixml', ['id' => $url, 'title' => $activity->title, 'description' => $activity->content])->render();
+    	$zipper = new \Madnest\Madzipper\Madzipper;
+    	$zipper->zip($filename)
+    		->addString('index.html', $html)
+    		->addString('tincan.xml', $xml)
+    		->close();
+    	return 'public/xapifiles/'.Str::slug($activity->title, '-').'.zip';
+    }
 }
