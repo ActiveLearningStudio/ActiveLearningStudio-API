0127e81bd87c43666d21dd0f35c15990d2b5a521
Antonio Etayo <antonioetayo@gmail.com>
2021-07-06 07:37:41 -0400

    formatting

--- app/Services/CurrikiGo/LMSIntegrationService.php
+++ app/Services/CurrikiGo/LMSIntegrationService.php
@@ -51,22 +51,22 @@ class LMSIntegrationService implements LMSIntegrationServiceInterface
 		return $this->lms->publishProject($project);
 	}
 
-    /**
-     * Get XAPI file contents
-     * 
-     * @return string
-     */
-    public function getXAPIFile(Activity $activity)
-    {
-    	$filename = 'storage/xapifiles/'.Str::slug($activity->title, '-').'.zip';
-    	$url = config('app.front_end_url').'/activity/'.$activity->id.'/shared';
-    	$html = view('api.xapihtml', ['url' => $url, 'title' => $activity->title])->render();
-    	$xml = view('api.xapixml', ['id' => $url, 'title' => $activity->title, 'description' => $activity->content])->render();
-    	$zipper = new \Madnest\Madzipper\Madzipper;
-    	$zipper->zip($filename)
-    		->addString('index.html', $html)
-    		->addString('tincan.xml', $xml)
-    		->close();
-    	return 'public/xapifiles/'.Str::slug($activity->title, '-').'.zip';
+	/**
+	* Get XAPI file contents
+	* 
+	* @return string
+	*/
+	public function getXAPIFile(Activity $activity)
+	{
+		$filename = 'storage/xapifiles/' . Str::slug($activity->title, '-') . '.zip';
+		$url = config('app.front_end_url').'/activity/'.$activity->id.'/shared';
+		$html = view('api.xapihtml', ['url' => $url, 'title' => $activity->title])->render();
+		$xml = view('api.xapixml', ['id' => $url, 'title' => $activity->title, 'description' => $activity->content])->render();
+		$zipper = new \Madnest\Madzipper\Madzipper;
+		$zipper->zip($filename)
+			->addString('index.html', $html)
+			->addString('tincan.xml', $xml)
+			->close();
+		return 'public/xapifiles/' . Str::slug($activity->title, '-') . '.zip';
     }
 }
