bacd88829467d126393c988b969e10045f28f445
Fahad <fahad.curriki@nxvt.com>
2021-07-20 21:33:41 +0500

    Refactored search response, added author data. Added search by author "first_name", "last_name" and "email".

--- app/Http/Resources/V1/SearchResource.php
+++ app/Http/Resources/V1/SearchResource.php
@@ -16,9 +16,20 @@ class SearchResource extends JsonResource
      */
     public function toArray($request)
     {
+        if (isset($this->playlist_id)) {
+            $author = $this->playlist->project->user;
+            $projectId = $this->playlist->project_id;
+        } elseif (isset($this->project_id)) {
+            $author = $this->project->user;
+            $projectId = $this->project_id;
+        } else {
+            $author = $this->user;
+            $projectId = $this->id;
+        }
+
         return [
             'id' => $this->id,
-            'project_id' => isset($this->playlist_id) ? $this->playlist->project_id : (isset($this->project_id) ? $this->project_id : $this->id),
+            'project_id' => $projectId,
             'playlist_id' => $this->when(isset($this->playlist_id), $this->playlist_id),
             'thumb_url' => (isset($this->thumb_url) ? $this->thumb_url : $this->thumbUrl),
             'title' => (isset($this->title) ? $this->title : $this->name),
@@ -26,7 +37,8 @@ class SearchResource extends JsonResource
             'content' => $this->when(isset($this->content), $this->content),
             'favored' => $this->when(isset($this->favored), $this->favored),
             'model' => $this->modelType,
-            'created_at' => $this->created_at
+            'created_at' => $this->created_at,
+            'author' =>  new SearchUserResource($author)
         ];
     }
 }
