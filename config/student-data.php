948c81975f12b944ed0877a219d669b869951677
basit-tkxel <80027948+basit-tkxel@users.noreply.github.com>
2021-10-01 20:00:45 +0500

    [VIV-226] Trim Values For DB Query (#619)

--- config/student-data.php
+++ config/student-data.php
@@ -2,8 +2,6 @@
 
 return [
     'save_student_data' => env('SAVE_STUDENT_DATA', false),
-    'run_dev_proc' => env('RUN_DEV_PROC'),
-    'run_prod_proc' => env('RUN_PROD_PROC'),
-    'dev_proc' => env('DEV_PROC'),
-    'prod_proc' => env('PROD_PROC'),
+    'run_dev_proc' => env('RUN_DEV_PROC', true),
+    'run_prod_proc' => env('RUN_PROD_PROC', false)
 ];
