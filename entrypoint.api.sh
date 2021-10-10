2395dc252c1794999cf7e025db0139329e9a6002
Ali Mehdi <imcoder001@gmail.com>
2021-10-10 12:18:51 +0500

    Update entrypoint.api.sh

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # service apache2 restart &
-chmod 777 -R /var/www/html/storage
+chmod 777 -R /var/www/html/storage &
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan storage:link
 if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage/oauth-public.key ]]; then php /var/www/html/artisan passport:install; fi
 
 php /var/www/html/artisan migrate --force
+
+sh /var/www/newrelic-php5-9.18.1.303-linux/newrelic-install install
 
 # service cron start &
 redis-server &
