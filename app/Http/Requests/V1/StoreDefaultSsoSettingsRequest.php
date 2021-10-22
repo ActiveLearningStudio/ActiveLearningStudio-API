3845f99de5466568fbc7723db769afc655d58452
Mohsin Tkxel <88667361+mohsin-tkxel@users.noreply.github.com>
2021-10-22 19:16:40 +0500

    Unique LTI Client id validation on Default SSO Integration apply (#651)
    * Unique id change for sso login
    
    * Apply unquie validation on LTI_client_id on Default SSO setting

--- app/Http/Requests/V1/StoreDefaultSsoSettingsRequest.php
+++ app/Http/Requests/V1/StoreDefaultSsoSettingsRequest.php
@@ -27,7 +27,7 @@ class StoreDefaultSsoSettingsRequest extends FormRequest
             'lms_url' => 'required|url|max:255',
             'lms_access_token' => 'required|min:20|max:255',
             'site_name' => 'required|string|max:255',
-            'lti_client_id' => 'nullable|string|max:255',
+            'lti_client_id' => 'nullable|string|max:255|unique:default_sso_integration_settings,lti_client_id,NULL,id,deleted_at,NULL',
             'lms_name' => 'nullable|string|max:255',
             'lms_access_key' => 'nullable|string|max:255',
             'lms_access_secret' => 'required_with:lms_access_key|max:255',
