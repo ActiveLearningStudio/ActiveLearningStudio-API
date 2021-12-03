ef9059bbdef51c048feec6f0ba2c05a0413000af
Shikha <shikhawebworks@gmail.com>
2021-12-03 16:02:51 +0530

    Add project and activity thumbnails in base64 format to zip file

--- app/Http/Controllers/Api/V1/ProjectDownloadController.php
+++ app/Http/Controllers/Api/V1/ProjectDownloadController.php
@@ -41,34 +41,37 @@ class ProjectDownloadController extends Controller
     public function exportProject(Request $request, Organization $suborganization, Project $project)
     {
         $zip = new ZipArchive;
-        // $h5p = App::make('LaravelH5p');
-        // $core = $h5p::$core;
-        // $settings = $h5p::get_editor();
         $user = Auth::user();
 
-        $project_dir_name = 'projects-'.uniqid();
-        Storage::disk('public')->put('/exports/'.$project_dir_name.'/project.json', $project);
-        
-        $project_thumbanil = "";
+        // Check if Project Thumbnail exists
         if (filter_var($project->thumb_url, FILTER_VALIDATE_URL) == false) {
             $project_thumbanil =  storage_path("app/public/" . (str_replace('/storage/', '', $project->thumb_url)));
-            $ext = pathinfo(basename($project_thumbanil), PATHINFO_EXTENSION); 
-            if(file_exists($project_thumbanil)) {
-                Storage::disk('public')->put('/exports/'.$project_dir_name.'/'.basename($project_thumbanil),file_get_contents($project_thumbanil));
-            }
+            $ext = pathinfo(basename($project_thumbanil), PATHINFO_EXTENSION);
+            $data = file_get_contents($project_thumbanil);
+            $project->thumb_url = 'data:image/' . $ext . ';base64,' . base64_encode($data);
         }
+        // Create Project.json file
+        $project_dir_name = 'projects-'.uniqid();
+        Storage::disk('public')->put('/exports/'.$project_dir_name.'/project.json', $project);
        
         $playlists = $project->playlists;
-        // return $playlists;
+        // get playlists from project
         foreach ($playlists as $playlist) {
-            
+            // Add playlist.json file
             $title = $playlist->title;
             Storage::disk('public')->put('/exports/'.$project_dir_name.'/playlists/'.$title.'/'.$title.'.json', $playlist);
             $activites = $playlist->activities;
             // return $activites;
             foreach($activites as $activity) {
-                Storage::disk('public')->put('/exports/'.$project_dir_name.'/playlists/'.$title.'/activities/'.$activity->title.'/'.$activity->title.'.json', $activity);
-                //dd(json_decode($activity->h5p_content,true));
+                // Check if activity has the thumbnails
+                if (filter_var($activity->thumb_url, FILTER_VALIDATE_URL) == false) {
+                    $activity_thumbanil =  storage_path("app/public/" . (str_replace('/storage/', '', $activity->thumb_url)));
+                    $activity_ext = pathinfo(basename($activity_thumbanil), PATHINFO_EXTENSION);
+                    $activity->thumb_url = 'data:image/' . $activity_ext . ';base64,' . base64_encode(file_get_contents($activity_thumbanil));
+                }
+                // Add activity.json file
+                Storage::disk('public')->put('/exports/'.$project_dir_name.'/playlists/'.$title.'/activities/'.$activity->title.'.json', $activity);
+                // Load h5p
                 $h5p = App::make('LaravelH5p');
                 $core = $h5p::$core;
                 $settings = $h5p::get_editor();
@@ -90,46 +93,11 @@ class ProjectDownloadController extends Controller
                 $decoded_content['library_minor_version'] = \DB::table('h5p_libraries')->where('id', $decoded_content['library_id'])->value('minor_version');
 
                 Storage::disk('public')->put('/exports/'.$project_dir_name.'/playlists/'.$title.'/activities/'.$activity->title.'/'.$activity->h5p_content_id.'.json', json_encode($decoded_content));
-                
-                if (filter_var($activity->thumb_url, FILTER_VALIDATE_URL) == false) {
-                    $activity_thumbanil =  storage_path("app/public/" . (str_replace('/storage/', '', $activity->thumb_url)));
-                    $ext = pathinfo(basename($activity_thumbanil), PATHINFO_EXTENSION); 
-                    // return file_exists($activity_thumbanil);
-                    // print_r($activity_thumbanil);
-                    if(file_exists($activity_thumbanil)) {
-                        Storage::disk('public')->put('/exports/'.$project_dir_name.'/playlists/'.$title.'/activities/'.$activity->title.'/'.basename($activity_thumbanil),file_get_contents($activity_thumbanil));
-                    }
-                }
+
                 \File::copyDirectory( storage_path('app/public/h5p/content/'.$activity->h5p_content_id), storage_path('app/public/exports/'.$project_dir_name.'/playlists/'.$title.'/activities/'.$activity->title.'/'.$activity->h5p_content_id) );
                 // return $activity->h5p_content_id;
             } 
-            // return storage_path('app/public/exports/'.$project_dir_name);
         }
-
-        // $h5p = App::make('LaravelH5p');
-        // $core = $h5p::$core;
-        // $settings = $h5p::get_editor();
-        // $content = $h5p->load_content(342);
-        // $content['enable'] = 8; // always enable H5P frame which include 'Reuse' and 'Embed' links
-        // $embed = $h5p->get_embed($content, $settings);
-        // $embed_code = $embed['embed'];
-        // $settings = $embed['settings'];
-        // $user = Auth::user();
-
-        // event(new H5pEvent(
-        //     'content',
-        //     NULL,
-        //     $content['id'],
-        //     $content['title'],
-        //     $content['library']['name'],
-        //     $content['library']['majorVersion'] . '.' . $content['library']['minorVersion']
-        // ));
-
-        // return response()->json([
-        //     'settings' => $settings,
-        //     'user' => $user,
-        //     'embed_code' => $embed_code
-        // ]);
         
         // Get real path for our folder
         $rootPath = storage_path('app/public/exports/'.$project_dir_name);
