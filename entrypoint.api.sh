34b36fb7e7675f0a66d1f2db0d0ae4763ddefa8a
Ali Mehdi <imcoder001@gmail.com>
2021-12-07 21:16:01 +0500

    Update entrypoint.api.sh

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-chmod 777 -R /var/www/html/storage &
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan storage:link
 if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage/oauth-public.key ]]; then php /var/www/html/artisan passport:install; fi
@@ -11,7 +10,7 @@ sh /var/www/newrelic-php5-9.18.1.303-linux/newrelic-install install
 git clone https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/htp-dist
 
 cp -rf /tmp/htp-dist/* /var/www/html/storage/app/public/h5p/libraries/
-
+chmod 777 -R /var/www/html/storage &
 touch /var/www/html/health.ok
 
 apache2ctl -D FOREGROUND
