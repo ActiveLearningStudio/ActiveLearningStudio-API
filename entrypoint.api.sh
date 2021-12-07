ddb931ad517f6660f5f041874a29cdb59467a224
Ali Mehdi <imcoder001@gmail.com>
2021-12-07 19:52:32 +0500

    Update entrypoint.api.sh

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# service apache2 restart &
 chmod 777 -R /var/www/html/storage &
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan storage:link
@@ -10,11 +9,7 @@ php /var/www/html/artisan migrate --force
 
 sh /var/www/newrelic-php5-9.18.1.303-linux/newrelic-install install
 git clone https://github.com/ActiveLearningStudio/h5p-course-presentation.git /var/www/html/storage/app/public/h5p-course-presentation-ali
-# service cron start &
-redis-server &
-# sleep 20
-# laravel-echo-server start --force &
-# php /var/www/html/artisan queue:work --timeout=0 &
+
 touch /var/www/html/health.ok
 
 apache2ctl -D FOREGROUND
