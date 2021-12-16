1e00fc2a4f465e6ac943fe69e295b54b31a9c40d
aqeel-tkxel <aqeel.ahmad@tkxel.com>
2021-12-16 11:38:35 +0500

    [CUR 2529] playlist id removed from tool url

--- app/CurrikiGo/Moodle/Playlist.php
+++ app/CurrikiGo/Moodle/Playlist.php
@@ -33,7 +33,7 @@ class Playlist
             "parent_name" => $playlist->project->name,
             "parent_type" => "program",
             "project_id" => $playlist->project_id,
-            "tool_url" => config('constants.curriki-tsugi-host') . "?playlist=" . $playlist->id
+            "tool_url" => config('constants.curriki-tsugi-host')
         ];
         $response = $this->client->request('GET', $web_service_url, ['query' => $rquest_params]);
         return $response;
