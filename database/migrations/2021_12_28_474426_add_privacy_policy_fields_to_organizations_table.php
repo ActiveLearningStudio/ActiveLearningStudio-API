39ff100cb39aae7601b661d03ddbadf5740428cf
Muhammad Waleed <74918344+waleed-backend@users.noreply.github.com>
2021-12-29 16:26:48 +0500

    Implemented ToS and privacy policy (#795)
    Co-authored-by: Muhammad Waleed <waleed.curriki@gmail.com>

---
+++ database/migrations/2021_12_28_474426_add_privacy_policy_fields_to_organizations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPrivacyPolicyFieldsToOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->enum('tos_type', ['Parent', 'URL', 'Content'])->nullable();
+            $table->string('tos_url')->nullable();
+            $table->text('tos_content')->nullable();
+            $table->enum('privacy_policy_type', ['Parent', 'URL', 'Content'])->nullable();
+            $table->string('privacy_policy_url')->nullable();
+            $table->text('privacy_policy_content')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn([
+                                'tos_type', 'tos_url', 'tos_content',
+                                'privacy_policy_type', 'privacy_policy_url', 'privacy_policy_content'
+                               ]);
+        });
+    }
+}
