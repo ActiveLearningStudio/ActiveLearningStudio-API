2f7c412689a9c5ed898cdf96f04abb75040a2307
asimsarwar <asim.softwareengg@gmail.com>
2021-12-30 11:46:45 +0500

    Implement CRUD API for Brightcove Integration - Backend Work (#794)
    * Implement CRUD APIs for Brightcove API Settings Integrations inside admin panel.
    
    * Update PR changes.

--- app/Repositories/Integration/BrightcoveAPISettingRepository.php
+++ app/Repositories/Integration/BrightcoveAPISettingRepository.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @author        Asim Sarwar
- * Date           09-12-2021
  * Class          BrightcoveAPISettingRepository
  */
 namespace App\Repositories\Integration;
@@ -55,6 +54,113 @@ class BrightcoveAPISettingRepository extends BaseRepository implements Brightcov
             return $setting;
         }
         throw new GeneralException('Brightcove API setting not found.');
+    }
+
+    /**
+     * @param array $data, integer $suborganization
+     * @return mixed
+     */
+    public function getAll($data, $suborganization)
+    {
+        $perPage = isset($data['size']) ? $data['size'] : config('constants.default-pagination-per-page');
+        $query = $this->model->with(['user', 'organization']);
+        if (isset($data['query']) && $data['query'] !== '') {
+            $query->where(function ($query) use ($data) {
+                $query = $query->whereHas('user', function ($qry) use ($data) {
+                    $qry->where('email', 'iLIKE', '%' . $data['query'] . '%');
+                });
+                $query->orWhere('account_id', 'iLIKE', '%' . $data['query'] . '%');
+                $query->orWhere('account_name', 'iLIKE', '%' . $data['query'] . '%');
+                $query->orWhere('account_email', 'iLIKE', '%' . $data['query'] . '%');
+            });
+        }
+        return $query->where('organization_id', $suborganization->id)->paginate($perPage);
+    }
+
+    /**
+     * @param  array $data
+     * @return mixed
+     * @throws GeneralException
+     */
+    public function create($data)
+    {
+        try {
+            if ($createSetting = $this->model->create($data)) {
+                return ['message' => 'Brightcove API setting created successfully!', 'data' => $createSetting];
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        throw new GeneralException('Unable to create Brightcove API setting, please try again later!');
+    }
+
+    /**
+     * @param  array $data, integer $id
+     * @return mixed
+     * @throws GeneralException
+     */
+    public function update($id, $data)
+    {
+        try {
+            if ($this->find($id)->update($data)) {
+                return ['message' => 'Brightcove API setting updated successfully!', 'data' => $this->find($id)];
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        throw new GeneralException('Unable to update Brightcove API setting, please try again later!');
+    }
+
+    /**
+     * @param  integer $id
+     * @return mixed
+     * @throws GeneralException
+     */
+    public function find($id)
+    {
+        if ($setting = $this->model->find($id)) {
+            return $setting;
+        }
+        throw new GeneralException('Brightcove API setting not found.');
+    }
+
+    /**
+     * @param  integer $id
+     * @return mixed
+     * @throws GeneralException
+     */
+    public function destroy($id)
+    {
+        try {
+            $this->find($id)->delete();
+            return ['message' => 'Brightcove API setting deleted!', 'data' => []];
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        throw new GeneralException('Unable to delete Brightcove API setting, please try again later!');
+    }
+
+    /**
+     * To clone Brightcove API Setting
+     * @param BrightcoveAPISetting $brightcoveAPISetting
+     * @param Organization $subOrganization
+     * @param string $token
+     * @return int id
+     */
+    public function clone(BrightcoveAPISetting $brightcoveAPISetting, Organization $subOrganization, $token)
+    {
+        $brightcoveAPISettingData = [
+            "user_id" => get_user_id_by_token($token),
+            "organization_id" => $subOrganization->id,
+            "account_id" => $brightcoveAPISetting->account_id,
+            "account_name" => $brightcoveAPISetting->account_name,
+            "account_email" => $brightcoveAPISetting->account_email,
+            "client_id" => $brightcoveAPISetting->client_id,
+            "client_secret" => $brightcoveAPISetting->client_secret,
+            "description" => $brightcoveAPISetting->description
+        ];
+        $clonedSetting = $this->create($brightcoveAPISettingData);
+        return $clonedSetting['id'];
     }
 
 }
