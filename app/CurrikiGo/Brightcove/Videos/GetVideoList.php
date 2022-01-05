4f50aa4739fbc601a8922e441bf061b8ba276b17
asimsarwar <asim.softwareengg@gmail.com>
2022-01-05 18:40:41 +0500

    Quick fix for Brightcove Search API. (#805)

--- app/CurrikiGo/Brightcove/Videos/GetVideoList.php
+++ app/CurrikiGo/Brightcove/Videos/GetVideoList.php
@@ -37,15 +37,39 @@ class GetVideoList
      */
     public function fetch($setting, $queryParam = '')
     {
+        $savedVideoCount = count($setting->h5pBrightcoveVideoContents);
         $getToken = $this->bcAPIClient->run(new GetAPITokenCommand($setting));
         if ( isset($getToken['Authorization']) ) {
             $getCountResponse = $this->bcAPIClient->run(new GetVideoCountCommand($setting, $getToken, $queryParam));
+            $getCountResponse['count'] = $getCountResponse['count'] - $savedVideoCount;
             $response = $this->bcAPIClient->run(new GetVideoListCommand($setting, $getToken, $queryParam));
             if ( $getCountResponse && $response) {
+                if ($savedVideoCount > 0) {
+                    $videoIds = $setting->h5pBrightcoveVideoContents->pluck('brightcove_video_id')->toArray();
+                    $response =  $this->filterVideos($response, $videoIds);    
+                }
                 return response()->json(['data' => $response, 'meta' => $getCountResponse]);
             } else {
                 throw new GeneralException('No Record Found!');
             }            
         }
     }
+
+    /**
+     * Exclude videos from Brightcove Search API Result
+     * @param  array $array, $videoIds
+     * @return array
+     */
+    private function filterVideos($array, $videoIds)
+    {
+        $filterVideos = [];
+        foreach ($array as $key => $value) {
+            if (!in_array($value['id'], $videoIds)) {
+                array_push($filterVideos, $value);
+                array_reverse($filterVideos);
+            }
+        }
+        return $filterVideos;
+    }
+
 }
