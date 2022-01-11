e64e99bbb027cb14545c548883c65766d150e970
Mahmood Ali Waseeem <63727438+shaheen7890@users.noreply.github.com>
2022-01-11 19:02:30 +0500

    Migration for essay submit button (#813)
    * Migration for essay submit button
    
    * fixed issues on PR
    
    Co-authored-by: Mahmood <mahmood.westanalytics@nxvt.com>

---
+++ database/migrations/2022_01_11_111044_update_essay_semaintics_to_h5p_libraries.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateEssaySemainticsToH5pLibraries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PEssaysLibSubmitButtonSeeder::class
+        ]);
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
