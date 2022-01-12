74efab4eb89b87e0a193b5f37d0744f37b645030
yuvaraj-curriki <90148816+yuvaraj-curriki@users.noreply.github.com>
2022-01-12 14:20:28 +0530

    CUR 2902 - Added submit button in UI for image sequence standalone activity (#819)

---
+++ database/migrations/2022_01_12_175579_update_image_sequence_semantics_to_h5p_libraries.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateImageSequencingSemanticsToH5PLibraries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PImageSequencingLibSubmitButtonSeeder::class
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
