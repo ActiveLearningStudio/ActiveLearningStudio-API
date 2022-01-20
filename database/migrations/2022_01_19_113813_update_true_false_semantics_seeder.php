e44ed792166bc63f129ca11678dbc315ad7980d6
Mahmood Ali Waseeem <63727438+shaheen7890@users.noreply.github.com>
2022-01-20 14:20:27 +0500

    seeder migration for true false semantics (#845)
    * seeder migration for true false semantics
    
    * seeder migration for true false semantics
    
    Co-authored-by: Mahmood <mahmood.westanalytics@nxvt.com>

---
+++ database/migrations/2022_01_19_113813_update_true_false_semantics_seeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTrueFalseSemanticsSeeder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PTrueFalseSubmitButtonSeeder::class
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
