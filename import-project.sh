eaa8bcb49b6579c3d48ac658d6b7b514e9ec082d
Ali Mehdi <imcoder001@gmail.com>
2022-02-10 16:07:06 +0500

    imported shell script

--- import-project.sh
+++ import-project.sh
@@ -2,29 +2,35 @@
 
 # list all folders
 # folders=$(ls -l /var/www/html/storage) # Find original_size / names
-base_path=/var/www/html/storage/
+base_path=/imported-projects/
 for i in $(ls ${base_path}imported-projects); do
-  for j in $(ls ${base_path}imported-projects/$i); do
-    # json_file = $(find ${base_path}imported-projects/$i/$j -iname *.json)
-    echo $(jq  '.size' ${base_path}imported-projects/$i/$j)
-  done
+    json_file=$(ls ${base_path}imported-projects/$i | grep json)
+    file_size=$(jq '.size' ${base_path}imported-projects/$i/$json_file | tr -d '"')
+    
+
+    zip_file=$(ls ${base_path}imported-projects/$i | grep -v json)
+    original_size=$(stat --printf="%s" ${base_path}imported-projects/$i/$zip_file)
+    echo  $file_size
+    echo $original_size
+    if [ $file_size == $original_size ]
+    then
+        echo "Size are equal"
+        response=$(/usr/local/bin/php /var/www/html/artisan import:project ${base_path}imported-projects/$i/$zip_file)
+        # response="{\"success\":false}"
+        success=$(jq '.success'  <<< $response)
+        # echo $response
+        # echo $success
+        if [ $success == 'true' ]
+        
+        then
+            echo 'trueeing'
+            # mv ${base_path}imported-projects/$i ${base_path}success-imports
+        else
+            echo 'falseeing'
+            # mv ${base_path}imported-projects/$i ${base_path}error-imports
+        fi
+    else
+        echo "Size are not equal.. Continue..."
+    fi
   
 done
-
-# list all files inside the folders
-
-# for (files in folders)
-# {
-#     json_file = file.json
-#     required_size = jq  '.size' json_file
-#     if(required_size == original_size){
-#         response = php artisan import:project /var/www/html/storage/.file_name
-#         if (response.success){
-#             mv file_name processed
-#         } else {
-#             mv file_name errors
-#         }
-        
-#     }
-# }
-
