e8546717f7a520ffaf956f0496ad965849c0433c
Mahmood Ali Waseeem <63727438+shaheen7890@users.noreply.github.com>
2022-03-08 16:51:12 +0500

    Added migrations for missing libraries (#932)
    Co-authored-by: Mahmood <mahmood.westanalytics@nxvt.com>

---
+++ database/migrations/2022_03_03_112558_seeder_add_semantic_star_rating_h5p.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class SeederAddSemanticStarRatingH5p extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PStarRatingSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_03_08_105129_add_bright_cove_editor_h5p_library.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+
+class AddBrightCoveEditorH5pLibrary extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PBrightCoveEditorLibrarySeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
