1f8d709df97e49a2b09ec14f25f929eb57a0736a
Ali Mehdi <imcoder001@gmail.com>
2022-04-07 16:25:32 +0500

    updated h5p

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -7,9 +7,9 @@ if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage
 php /var/www/html/artisan migrate --force
 
 sh /var/www/newrelic-php5-9.18.1.303-linux/newrelic-install install
-# git clone -b develop https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/htp-dist
+git clone -b develop https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/h5p-dist
 
-# cp -rf /tmp/htp-dist/* /var/www/html/storage/app/public/h5p/libraries/
+# cp -rf /tmp/h5p-dist/* /var/www/html/storage/app/public/h5p/libraries/
 chmod 777 -R /var/www/html/storage &
 touch /var/www/html/health.ok
 
