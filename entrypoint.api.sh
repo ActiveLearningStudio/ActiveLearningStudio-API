d184c2e02b0362874c90d241f049d5025c231a38
IMCoder <imcoder001@gmail.com>
2022-04-12 23:36:33 +0500

    updated new relic

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -6,7 +6,15 @@ if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage
 
 php /var/www/html/artisan migrate --force
 
-sh /var/www/newrelic-php5-9.18.1.303-linux/newrelic-install install
+#New Relilc
+nr_enabled=$(cat /var/www/html/.env | grep ENABLE_NR);
+
+
+if [ "$nr_enabled" -eq "1" ]; then
+  sh /var/www/cartx-ecomm/newrelic-php5-9.20.0.310-linux/newrelic-install install
+  sed -i -e "s/newrelic.appname =.*/newrelic.appname = \"\Laravel API\"/" /usr/local/etc/php/conf.d/newrelic.ini
+fi
+
 git clone -b develop https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/htp-dist
 
 cp -rf /tmp/htp-dist/* /var/www/html/storage/app/public/h5p/libraries/
