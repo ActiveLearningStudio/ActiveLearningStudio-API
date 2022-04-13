e2399aca1feca20e15ecf3a8e77e2e22e65a6aea
Fahad Farrukh <fahad.farrukh@nxb.com.pk>
2022-04-13 16:44:55 +0500

    CUR-3560: Removed "SendDailyUsage" command from Kernel schedule on staging.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command(SendDailyUsage::class)->dailyAt('0:00');
-        $schedule->command(SendDailyUsage::class)->everyFourHours();
+        // $schedule->command(SendDailyUsage::class)->everyFourHours();
         $schedule->command(PushNoovo::class)->everyMinute()->withoutOverlapping()->runInBackground();
         $schedule->command(DeleteExportedProjects::class)->daily()->runInBackground();
     }
