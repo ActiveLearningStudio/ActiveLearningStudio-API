ed6bc47ffc70ac3218848609f671da24e6ca4909
Ali Mehdi <imcoder001@gmail.com>
2022-04-20 19:50:56 +0500

    updated cords

--- config/cors.php
+++ config/cors.php
@@ -27,7 +27,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [$domain, env('FRONT_END_URL')],
+    'allowed_origins' => [$domain[0], env('FRONT_END_URL')],
 
     'allowed_origins_patterns' => [],
 
