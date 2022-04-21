6369c37ba13c75dc9b97edd2a3dacd3c6e79fb64
Ali Mehdi <imcoder001@gmail.com>
2022-04-21 16:11:28 +0500

    updated cors and removed warning

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,14 @@
 <?php
 $allowed_cors = explode(",", env('APP_ALLOWED_URL'));
 
-$input = $_SERVER['HTTP_HOST'];
-$domain = array_filter($allowed_cors, function ($item) use ($input) {
+$input = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST']) : "";
+$domain_arr = array_filter($allowed_cors, function ($item) use ($input) {
     if (stripos($item, $input) !== false) {
         return true;
     }
     return false;
 });
+$domain = isset($domain_arr[0])? $domain_arr[0] : null;
 return [
 
     /*
