de95f290f21a166ea2992e3d1ec16a603a813236
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2022-06-01 12:16:39 +0530

    CUR-3570 - Upgrade MultiChoice to H5P.MultiChoice 1.16

---
+++ database/migrations/2022_06_01_134181_add_multichoice_h5p_library.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddMultichoiceH5pLibrary extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => H5PAddMultiChoiceLibrarySeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
