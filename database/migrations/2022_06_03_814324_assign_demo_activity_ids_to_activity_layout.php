d16b7a92bb0bb1d59f4f761849e0c2a267dd4398
Muhammad Waleed <74918344+waleed-backend@users.noreply.github.com>
2022-06-07 09:34:00 +0500

    [CUR-3717-] Assigned demo activity IDs and demo video IDs to default layouts (#1116)
    * Assigned demo activity IDs and demo video IDs to default layouts
    
    * added activity item seeder to associate demo activity and video IDs
    
    * updated activity item seeder for demo IDs
    
    * some changes
    
    * some changes
    
    * some changes
    
    Co-authored-by: Muhammad Waleed <waleed.curriki@gmail.com>

---
+++ database/migrations/2022_06_03_814324_assign_demo_activity_ids_to_activity_layout.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AssignDemoActivityIdsToActivityLayout extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => DemoActivityIdsAssociationSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
