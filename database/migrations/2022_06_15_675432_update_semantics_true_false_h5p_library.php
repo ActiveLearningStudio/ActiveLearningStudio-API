63b8cca3ea0ed9e2ec0d42302acafb6c9d4e4933
yuvaraj-curriki <90148816+yuvaraj-curriki@users.noreply.github.com>
2022-06-15 19:28:30 +0530

    Feature/cur 3750 upgrade truefalse 1.8 (#1138)
    * feature CUR-3750 - Upgrade H5P.TrueFalse from 1.6 to 1.8
    
    * Added Missing Curriki Settings

---
+++ database/migrations/2022_06_15_675432_update_semantics_true_false_h5p_library.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSemanticsTrueFalseH5pLibrary extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => UpdateSemanticsTrueFalseLibrarySeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
