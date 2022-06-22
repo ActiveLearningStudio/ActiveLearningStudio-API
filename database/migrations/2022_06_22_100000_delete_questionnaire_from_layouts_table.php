8320d2457924fa853cea3334408960359b3d255d
Muhammad Waleed <waleed.curriki@gmail.com>
2022-06-22 18:29:55 +0500

    managed demo activity and video IDs for newly created organziation and assign default admin permissions to all other orgs admins

---
+++ database/migrations/2022_06_22_100000_delete_questionnaire_from_layouts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteQuestionnaireFromLayoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => DeleteQuestionnaireFromLayoutsSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
