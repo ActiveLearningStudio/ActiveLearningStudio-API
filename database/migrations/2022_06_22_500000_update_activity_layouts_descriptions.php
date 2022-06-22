d08594f5e518793d0f8de17669dfbea77298b676
Muhammad Waleed <waleed.curriki@gmail.com>
2022-06-22 19:41:05 +0500

    updated layouts descriptions and update the order of items to 100

---
+++ database/migrations/2022_06_22_500000_update_activity_layouts_descriptions.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateActivityLayoutsDescriptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => UpdateActivityLayoutsDescriptionSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
