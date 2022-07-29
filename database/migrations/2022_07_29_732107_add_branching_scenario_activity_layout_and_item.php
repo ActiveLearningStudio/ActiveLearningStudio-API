f1fd1d75662b9c4848c6db53d20637cf8765b64e
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2022-07-29 14:16:57 +0530

    CUR-3568 - Add Branching Scenario to Curriki Studio as a Layout

---
+++ database/migrations/2022_07_29_732107_add_branching_scenario_activity_layout_and_item.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddBranchingScenarioActivityLayoutAndItem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => AddBranchingScenarioActivityLayoutAndItemSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
