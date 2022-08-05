569ffbe9ba9a844eb702973cc15a7c38c9d1a8bf
yuvaraj-curriki <90148816+yuvaraj-curriki@users.noreply.github.com>
2022-08-05 17:40:14 +0530

    1. Update H5P Library versions in Activity Layout & Activity Items (#1247)
    * 1. Update H5P Library versions in Activity Layout & Activity Items
    2. Update Demo activity id & video url for Branching Scenario
    
    * updated Logic as suggested

---
+++ database/migrations/2022_08_05_754680_upgrade_h5p_versions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpgradeH5pVersions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        \Artisan::call('db:seed', [
+            '--class' => UpgradeH5pVersionsSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
