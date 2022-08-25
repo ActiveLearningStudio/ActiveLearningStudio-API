6f22bf4e626201b4a4ed85cf35eaea726c4777df
AqibYounasAtTkxel <94671546+AqibYounasAtTkxel@users.noreply.github.com>
2022-08-25 17:12:58 +0500

    CUR 4004 commit to clear working tree

--- routes/api.php
+++ routes/api.php
@@ -287,6 +287,8 @@ Route::group(['prefix' => 'v1', 'namespace' => 'Api\V1'], function () {
             });
 
             Route::group(['prefix' => 'canvas'], function () {
+                Route::get('fetch-all-courses', 'CurrikiGo\CourseController@fetchMyCoursesFromCanvas');
+                Route::get('{courseId}/fetch-assignment-groups', 'CurrikiGo\CourseController@fetchAssignmentGroups');
                 Route::post('projects/{project}/playlists/{playlist}/publish', 'CurrikiGo\PublishController@playlistToCanvas');
                 Route::post('projects/{project}/fetch', 'CurrikiGo\CourseController@fetchFromCanvas');
             });
