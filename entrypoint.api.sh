71f81bd978bf420ce0401554b0476b88be084bd1
Ali Mehdi <alimehdi.official@gmail.com>
2022-09-09 16:03:56 +0500

    Update entrypoint.api.sh

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -11,7 +11,7 @@ sh /var/www/newrelic-php5-10.0.0.312-linux/newrelic-install install
 h5p_branch=$(printenv H5P_BRANCH);
 git clone -b $h5p_branch https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/h5p-dist
 
-cd /var/www/html && git --no-pager log -10 > /var/www/html/public/log.txt
+cd /var/www/html && git log --graph -10 --decorate --pretty > /var/www/html/public/log.txt
 cp -rf /tmp/h5p-dist/* /var/www/html/storage/app/public/h5p/
 chmod 777 -R /var/www/html/storage &
 touch /var/www/html/health.ok
