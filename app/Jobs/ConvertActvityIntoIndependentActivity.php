27fa3692d02464abbb805e553b0b4f2ae95070d6
fahad-curriki <63652626+fahad-curriki@users.noreply.github.com>
2022-09-27 16:23:09 +0500

    Merge pull request #1327 from ActiveLearningStudio/feature/CUR-4138-code-update-for-standalone-activities
    Feature/cur 4138 code update for standalone activities

--- app/Jobs/ConvertActvityIntoIndependentActivity.php
+++ app/Jobs/ConvertActvityIntoIndependentActivity.php
@@ -16,6 +16,7 @@ use App\Notifications\CloneNotification;
 use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\Facades\Log;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class ConvertActvityIntoIndependentActivity implements ShouldQueue
 {
@@ -62,16 +63,14 @@ class ConvertActvityIntoIndependentActivity implements ShouldQueue
      * @return void
      */
     public function handle(
-        IndependentActivityRepositoryInterface $independentActivityRepository, 
+        IndependentActivityRepositoryInterface $independentActivityRepository,
         UserRepositoryInterface $userRepository
-    )
-    {
+    ) {
         try {
             $independentActivityRepository->convertIntoIndependentActivity($this->organization, $this->activity, $this->token);
             $message = "Your request to copy activity [" . $this->activity->title . "] into 
                             Independent activity has been completed and available";
-            $userId = $userRepository->parseToken($this->token);
-            $user = User::find($userId);
+            $user = User::find(Auth::id());
             $userName = rtrim($user->first_name . ' ' . $user->last_name, ' ');
             $process = "Copy Activity into Independent Activity";
             $user->notify(new CloneNotification($message, $process, $userName));
