fb7302a9826c42124cb5489c0ec7ea8b752ab9a0
Asim Sarwar <asim.softwareengg@gmail.com>
2022-09-28 16:02:47 +0500

    CUR-4097 update config file.

--- config/smithsonian.php
+++ config/smithsonian.php
@@ -6,12 +6,12 @@ return [
     | Smithsonian API Base Url
     |--------------------------------------------------------------------------
     */
-    'api_base_url' => env('Smithsonian_API_BASE_URL', 'https://api.si.edu/openaccess/api/v1.0'),
+    'api_base_url' => env('SMITHSONIAN_API_KEY', 'https://api.si.edu/openaccess/api/v1.0'),
 
     /*
     |--------------------------------------------------------------------------
     | Smithsonian API Key
     |--------------------------------------------------------------------------
     */
-    'api_key' => env('Smithsonian_API_KEY', 'invpQb67otWXfxjTj8EYXd77UeCLnSguDczIni5A')
+    'api_key' => env('SMITHSONIAN_API_KEY', 'invpQb67otWXfxjTj8EYXd77UeCLnSguDczIni5A')
 ];
