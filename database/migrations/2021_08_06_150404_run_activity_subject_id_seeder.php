1db32f7d158138fabd03b87c2eadeb61ffb7571c
Fahad Farrukh <fahad.farrukh@nxb.com.pk>
2022-09-28 13:06:44 +0500

    CUR-4118: Added migration for "ActivitySubjectIdSeeder" to run it on deployment.

---
+++ database/migrations/2021_08_06_150404_run_activity_subject_id_seeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class RunActivitySubjectIdSeeder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => ActivitySubjectIdSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
