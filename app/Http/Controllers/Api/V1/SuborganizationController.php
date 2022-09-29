85bc93e2f44b353d669e40153102e10a94cbe891
Mahmood Ali Waseem <mahmood.curriki@nxvt.com>
2022-09-29 14:00:11 +0500

    CUR-4179- pr review changes

--- app/Http/Controllers/Api/V1/SuborganizationController.php
+++ app/Http/Controllers/Api/V1/SuborganizationController.php
@@ -184,10 +184,10 @@ class SuborganizationController extends Controller
      * @bodyParam tertiary_color string primary font color Example: #515151
      * @bodyParam primary_font_family string primary font color Example: Open Sans
      * @bodyParam secondary_font_family string primary font color Example: Open Sans
-     * @bodyParam msteam_client_id string client id Example: 123e4567-e89b-12d3-a456-426614174000
-     * @bodyParam msteam_secret_id string secret id Example: 123e4567-e89b-12d3-a456-426614174000
-     * @bodyParam msteam_tenant_id string tenant id Example: 123e4567-e89b-12d3-a456-426614174000
-     * @bodyParam msteam_secret_id_expiry date secret expiry date Example: 2022-09-29
+     * @bodyParam msteam_client_id uuid Client id Example: 123e4567-e89b-12d3-a456-426614174000
+     * @bodyParam msteam_secret_id uuid Secret id Example: 123e4567-e89b-12d3-a456-426614174000
+     * @bodyParam msteam_tenant_id uuid Tenant id Example: 123e4567-e89b-12d3-a456-426614174000
+     * @bodyParam msteam_secret_id_expiry date Secret expiry date Example: 2022-09-29
      *
      * @responseFile 201 responses/organization/suborganization.json
      *
@@ -271,10 +271,10 @@ class SuborganizationController extends Controller
      * @bodyParam tertiary_color string primary font color Example: #515151
      * @bodyParam primary_font_family string primary font color Example: Open Sans
      * @bodyParam secondary_font_family string primary font color Example: Open Sans
-     * @bodyParam msteam_client_id uuid client id Example: 123e4567-e89b-12d3-a456-426614174000
-     * @bodyParam msteam_secret_id uuid secret id Example: 123e4567-e89b-12d3-a456-426614174000
-     * @bodyParam msteam_tenant_id uuid tenant id Example: 123e4567-e89b-12d3-a456-426614174000
-     * @bodyParam msteam_secret_id_expiry date secret expiry date Example: 2022-09-29
+     * @bodyParam msteam_client_id uuid Client id Example: 123e4567-e89b-12d3-a456-426614174000
+     * @bodyParam msteam_secret_id uuid Secret id Example: 123e4567-e89b-12d3-a456-426614174000
+     * @bodyParam msteam_tenant_id uuid Tenant id Example: 123e4567-e89b-12d3-a456-426614174000
+     * @bodyParam msteam_secret_id_expiry date Secret expiry date Example: 2022-09-29
      *
      * @responseFile responses/organization/suborganization.json
      *
