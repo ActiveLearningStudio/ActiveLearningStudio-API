e9097e24bfd12e83b499fb3a26985a521821637a
Ali Mehdi <alimehdi.official@gmail.com>
2022-10-13 11:37:34 +0500

    Update entrypoint.api.sh

--- entrypoint.api.sh
+++ entrypoint.api.sh
@@ -6,12 +6,20 @@ if [[ ! -e /var/www/html/storage/oauth-private.key || ! -e /var/www/html/storage
 
 php /var/www/html/artisan migrate --force
 
-sh /var/www/newrelic-php5-10.0.0.312-linux/newrelic-install install
+#New Relilc
+nr_enabled=$(printenv ENABLE_NR);
+if [ "$nr_enabled" -eq "1" ]; then
+  export NR_INSTALL_SILENT=true
+  export NR_INSTALL_KEY=$(printenv NR_INSTALL_KEY)
+  export nr_name=$(printenv NR_NAME)
+  sh /var/www/html/newrelic-php5-10.2.0.314-linux/newrelic-install install
+  sed -i -e "s/newrelic.appname =.*/newrelic.appname = \"\Curriki-API $nr_name\"/" /usr/local/etc/php/conf.d/newrelic.ini
+fi
 
 h5p_branch=$(printenv H5P_BRANCH);
 git clone -b $h5p_branch https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/h5p-dist
 
-cd /var/www/html && git --no-pager log -10 > /var/www/html/public/log.txt
+cd /var/www/html && git log --graph -10 --decorate --pretty > /var/www/html/public/log.txt
 cp -rf /tmp/h5p-dist/* /var/www/html/storage/app/public/h5p/
 chmod 777 -R /var/www/html/storage &
 touch /var/www/html/health.ok
