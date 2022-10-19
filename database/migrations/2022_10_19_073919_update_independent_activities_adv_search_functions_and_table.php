72d66168c088e1a148107d077183274fb36bcc58
Fahad Farrukh <fahadfarrukh@Aleezas-MacBook-Air.local>
2022-10-19 21:10:10 +0500

    CUR-4068: Integrated SQL scripts for independent activity and project search.

--- database/migrations/2022_10_19_073919_update_independent_activities_adv_search_functions_and_table.php
+++ database/migrations/2022_10_19_073919_update_independent_activities_adv_search_functions_and_table.php
@@ -123,10 +123,12 @@ class UpdateIndependentActivitiesAdvSearchFunctionsAndTable extends Migration
                 cnd := cnd || ' and acts.subject_id in ' || _subject;
                 joinTable := joinTable || ' left join activity_subject acts on a.id=acts.activity_id ';
             end if;
+
             if _education != '' then 
                 cnd := cnd || ' and ael.education_level_id in ' || _education;
                 joinTable := joinTable || ' left join activity_education_level ael on a.id=ael.activity_id ';
             end if;
+
             if _tag != '' then 
                 cnd := cnd || ' and aat.author_tag_id in ' || _tag ;
                 joinTable := joinTable || ' left join activity_author_tag aat on a.id=aat.activity_id ';
@@ -145,7 +147,9 @@ class UpdateIndependentActivitiesAdvSearchFunctionsAndTable extends Migration
                 end loop;
                     h5p:=  h5p || split_part(split_part(_h5p,',',hlCnt+1),' ',1) || ''') ' ;
                 end if;
+                cnd := cnd || h5p;
             end if;
+
         query := format($s$ select distinct 1 as priority,'Independent Activity' as entity,a.organization_id as org_id,a.id as entity_id,a.user_id as user_id, null::bigint as project_id,
                 null::bigint as playlist_id,u.first_name,u.last_name,u.email,a.title as name,a.description as description,a.thumb_url,a.created_at,a.deleted_at,
                 a.shared as is_shared,a.is_public,a.indexing,a.organization_visibility_type_id, concat(concat(concat(hl.name,' '),major_version),concat('.',minor_version)) as h5pLib
@@ -200,10 +204,12 @@ class UpdateIndependentActivitiesAdvSearchFunctionsAndTable extends Migration
                 cnd := cnd || ' and acts.subject_id in ' || _subject;
                 joinTable := joinTable || ' left join activity_subject acts on a.id=acts.activity_id ';
             end if;
+
             if _education != '' then 
                 cnd := cnd || ' and ael.education_level_id in ' || _education;
                 joinTable := joinTable || ' left join activity_education_level ael on a.id=ael.activity_id ';
             end if;
+
             if _tag != '' then 
                 cnd := cnd || ' and aat.author_tag_id in ' || _tag ;
                 joinTable := joinTable || ' left join activity_author_tag aat on a.id=aat.activity_id ';
@@ -222,8 +228,9 @@ class UpdateIndependentActivitiesAdvSearchFunctionsAndTable extends Migration
                 end loop;
                     h5p:=  h5p || split_part(split_part(_h5p,',',hlCnt+1),' ',1) || ''') ' ;
                 end if;
-            
+            cnd := cnd || h5p;
             end if;
+
         query := format($s$ select distinct 1 as priority,'Independent Activity' as entity,a.organization_id as org_id,a.id as entity_id,a.user_id as user_id, null::bigint as project_id,
                 null::bigint as playlist_id,u.first_name,u.last_name,u.email,a.title as name,a.description as description,a.thumb_url,a.created_at,a.deleted_at,
                 a.shared as is_shared,a.is_public,a.indexing,a.organization_visibility_type_id, concat(concat(concat(hl.name,' '),major_version),concat('.',minor_version)) as h5pLib
