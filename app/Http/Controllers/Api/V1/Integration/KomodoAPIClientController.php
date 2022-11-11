f5d50d1e797a8239e958127e2decda73c410ecbd
asimsarwar <asim.softwareengg@gmail.com>
2022-11-11 21:53:53 +0500

    CUR-4316 update LTI Tool Settings as Org based functionality. (#1394)
    * CUR-4316 update LTI Tool Settings as Org based functionality.
    
    * CUR-4316 Updated PR suggested changes.
    
    * CUR-4316 Update PR requested changes.
    
    * CUR-4316 minor update
    
    * CUR-4316 Update changes.

--- app/Http/Controllers/Api/V1/Integration/KomodoAPIClientController.php
+++ app/Http/Controllers/Api/V1/Integration/KomodoAPIClientController.php
@@ -8,44 +8,61 @@ namespace App\Http\Controllers\Api\V1\Integration;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Repositories\LtiTool\LtiToolSettingRepository;
+use App\Repositories\LtiTool\LtiToolSettingInterface;
 use App\CurrikiGo\Komodo\Client;
 use App\CurrikiGo\Komodo\Videos\GetMyVideoList;
 use App\Exceptions\GeneralException;
-use App\Models\MediaSource;
+use App\Http\Requests\V1\LtiTool\KomodoAPISettingRequest;
+use App\Repositories\MediaSources\MediaSourcesInterface;
 
 class KomodoAPIClientController extends Controller
 {
     private $ltiToolSettingRepository;
+    private $mediaSourcesRepository;
     /**
      * KomodoAPIClientController constructor.
-     * @param LtiToolSettingRepository $ltiToolSettingRepository
+     * @param LtiToolSettingInterface $ltiToolSettingRepository
+     * @param MediaSourcesInterface $mediaSourcesRepository
      */
-    public function __construct(LtiToolSettingRepository $ltiToolSettingRepository)
+    public function __construct(LtiToolSettingInterface $ltiToolSettingRepository, MediaSourcesInterface $mediaSourcesRepository)
     {
         $this->ltiToolSettingRepository = $ltiToolSettingRepository;
+        $this->mediaSourcesRepository = $mediaSourcesRepository;
     }
 
     /**
      * Get My Komodo Videos List
-     * Get the specified Komodo API setting data.
-     * @param Request $request
+     *
+     * Get the specified Komodo API setting data. Using inside H5p Curriki Interactive Video
+     *
+     * @bodyParam organization_id required int The Id of a suborganization Example: 1
+     * @bodyParam page required string Mean pagination or page number Example: 1
+     * @bodyParam per_page required string Mean record per page Example: 10next page and so on
+     * @bodyParam search optional string Mean search record by video title Example: Wildlife
+     *
+     * @responseFile responses/komodo/komodo-videos.json
+     *
+     * @response 500 {
+     *   "errors": [
+     *     "Unable to find Komodo API settings!"
+     *   ]
+     * }
+     *
+     * @param KomodoAPISettingRequest $request
      * @return object $response
      * @throws GeneralException
      */
-    public function getMyVideosList(Request $request)
+    public function getMyVideosList(KomodoAPISettingRequest $request)
     {
-      $getParam = $request->only([
-        'organization_id',
-        'page',
-        'per_page',
-        'search'
-      ]);
-      $auth = \Auth::user();
-      $mediaSourcesRow = MediaSource::where('name', 'Komodo')->where('media_type', 'Video')->first();
-      $mediaSourcesId = ($mediaSourcesRow) ? $mediaSourcesRow->id : 0;
-      if ( $auth && $auth->id && isset($getParam['organization_id']) && $getParam['organization_id'] > 0 ) {
-        $ltiRowResult = $this->ltiToolSettingRepository->getRowRecordByUserOrgAndToolType($auth->id, $getParam['organization_id'], $mediaSourcesId);
+        $getParam = $request->only([
+            'organization_id',
+            'page', 
+            'per_page',
+            'search'
+        ]);
+        $videoMediaSources = getVideoMediaSources();
+      $mediaSourcesId = $this->mediaSourcesRepository->getMediaSourceIdByName($videoMediaSources['komodo']);
+      $ltiRowResult = $this->ltiToolSettingRepository->getRowRecordByOrgAndToolType($getParam['organization_id'], $mediaSourcesId);
         if ($ltiRowResult) {
             // Implement Command Design Pattern to access Komodo API
             unset($getParam['organization_id']);
@@ -54,8 +71,6 @@ class KomodoAPIClientController extends Controller
             $response = $instance->fetch($ltiRowResult, $getParam);
             return $response;
         }
-        throw new GeneralException('Unable to find komodo settings!');
-      }
-      throw new GeneralException('Unable to get the record. Please check your payload data. organization_id, page and per_page is require field!');      
+        throw new GeneralException('Unable to find Komodo API settings!');
     }
 }
