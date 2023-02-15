6016673edab2392ae9d0bcc46187a169a145798c
Ali Mehdi <imcoder001@gmail.com>
2023-02-15 23:53:44 +0500

    env added

--- scripts/entrypoint.api.sh
+++ scripts/entrypoint.api.sh
@@ -8,16 +8,16 @@ php /var/www/html/artisan storage:link
 
 #New Relilc
 
-nr_enabled=$(cat ../.env | grep ENABLE_NR |awk -F= '{print $2}');
+nr_enabled=$(cat /var/www/html/.env | grep ENABLE_NR |awk -F= '{print $2}');
 if [ "$nr_enabled" -eq "1" ]; then
   export NR_INSTALL_SILENT=true
-  export NR_INSTALL_KEY=$(cat ../.env | grep NR_INSTALL_KEY | awk -F= '{print $2}')
-  export nr_name=$(cat ../.env | grep NR_NAME | awk -F= '{print $2}')
+  export NR_INSTALL_KEY=$(cat /var/www/html/.env | grep NR_INSTALL_KEY | awk -F= '{print $2}')
+  export nr_name=$(cat /var/www/html/.env | grep NR_NAME | awk -F= '{print $2}')
   sh /var/www/html/newrelic/newrelic-install install
   sed -i -e "s/newrelic.appname =.*/newrelic.appname = \"\Curriki-API $nr_name\"/" /usr/local/etc/php/conf.d/newrelic.ini
 fi
 
-# h5p_branch=$(cat ../.env H5P_BRANCH);
+# h5p_branch=$(cat /var/www/html/.env H5P_BRANCH);
 # git clone -b $h5p_branch https://github.com/ActiveLearningStudio/H5P.Distribution.git /tmp/h5p-dist
 
 cd /var/www/html && git log --graph -10 --decorate --pretty > /var/www/html/public/log.txt
