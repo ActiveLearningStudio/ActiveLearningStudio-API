820a66b88adc4bc52e9fd60195cf6ba33142bb2b
AqibYounasAtTkxel <94671546+AqibYounasAtTkxel@users.noreply.github.com>
2023-02-16 14:38:54 +0500

    CUR 4646 MST get user profile api integration

---
+++ app/Http/Requests/V1/GetUserProfileRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetUserProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'code' => 'required'
+        ];
+    }
+}
