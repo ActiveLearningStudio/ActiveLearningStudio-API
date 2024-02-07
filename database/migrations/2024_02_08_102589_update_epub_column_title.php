4c4bc416d9415ed22a1ab6fd801b7ae20faa6bc9
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2024-02-08 03:07:57 +0530

    Update epub column layout title

---
+++ database/migrations/2024_02_08_102589_update_epub_column_title.php
@@ -0,0 +1,31 @@
+<?php
+
+
+use Database\Seeders\UpdateEpubColumnTitleSeeder;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateEpubColumnTitle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => UpdateEpubColumnTitleSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
