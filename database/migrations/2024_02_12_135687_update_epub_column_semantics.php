16ff479da8254e61c789abf7305850ccdf1225a4
Yuvaraj Sanjeevi <yuvaraj@curriki.org>
2024-02-12 02:16:04 +0530

    Replace Course Presentation with Questionnaire

---
+++ database/migrations/2024_02_12_135687_update_epub_column_semantics.php
@@ -0,0 +1,31 @@
+<?php
+
+
+use Database\Seeders\UpdateEpubColumnSemanticsSeeder;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateEpubColumnSemantics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Artisan::call('db:seed', [
+            '--class' => UpdateEpubColumnSemanticsSeeder::class,
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
